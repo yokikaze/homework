378d84989365310a1b2877452606970fdc7b53fa
lisijia <1419268805@qq.com>
2019-08-12 15:02:45 +0800

    Eureka-服务提供端

---
+++ eureka_client/src/main/java/com/ke/lisijia/eureka_client/controller/DcController.java
@@ -0,0 +1,24 @@
+package com.ke.lisijia.eureka_client.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class DcController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "services:" + discoveryClient.getServices();
+        log.info("{}", services);
+        return services;
+    }
+}
