872af52ddb55b635b38dc196122d21e263c87986
lisijia <1419268805@qq.com>
2019-08-12 14:25:36 +0800

    Eureka-服务注册中心

--- src/main/java/com/ke/lisijia/spring_boot_demo/SpringBootDemoApplication.java
+++ src/main/java/com/ke/lisijia/spring_boot_demo/SpringBootDemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableCaching
+@EnableEurekaServer
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
